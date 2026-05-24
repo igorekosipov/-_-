b81a041cc63c83dcb798598133860accd31e596a
igorekosipov <igorek-osipov-1996@mail.ru>
2026-05-24 18:52:53 +0700

    Refactor state definitions in states.py

--- states.py
+++ states.py
@@ -1,8 +1,7 @@
 from aiogram.fsm.state import State, StatesGroup
 
-# Это состояния - как этапы разговора с ботом
 class PaymentStates(StatesGroup):
-    waiting_for_receipt = State()  # Ждем когда пользователь отправит чек
+    waiting_for_receipt = State()
 
 class LotteryStates(StatesGroup):
-    waiting_for_admin_action = State()  # Ждем действие админа
+    waiting_for_admin_action = State()
